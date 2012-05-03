e6cb40e6de61be85c932727fe35a0044cbcff313
Thomas Gilgenast <thomasgilgenast@gmail.com>
2012-05-03 03:29:07 -0400

    added contracts urlconf

---
+++ contracts/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('shk.contracts.views',
+    #url(r'^exchange/$', 'exchange'),
+    #url(r'^ious/$', 'IOUs'),
+    #url(r'^contracts/$', 'contracts'),
+)
