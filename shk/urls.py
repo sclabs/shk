88f137a443db35e3adcd733487a21183e8ada883
Thomas Gilgenast <thomasgilgenast@gmail.com>
2012-05-13 05:13:52 -0400

    trying to save the admin

--- shk/urls.py
+++ shk/urls.py
@@ -39,6 +39,6 @@ urlpatterns = patterns('',
 if DEBUG:
     urlpatterns += patterns('',
         url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': MEDIA_ROOT,}),
-        url(r'^static/admin/(?P<path>.*)$', 'django.views.static.serve', {'document_root': STATIC_ROOT + '/admin/',}),
+        #url(r'^static/admin/(?P<path>.*)$', 'django.views.static.serve', {'document_root': STATIC_ROOT + '/admin/',}),
         url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': STATIC_ROOT,})
                             )
